d1d96469d8c6e5994b2876e8f90708fd6c9344c5
WKHAllen <wkhallen@gmail.com>
2023-07-26 20:50:03 -0400

    Add address example

---
+++ json-schema-macro/examples/address.rs
@@ -0,0 +1,75 @@
+use json_schema_macro::*;
+use serde_json::{json, Map, Value};
+
+enum AddressType {
+    US,
+    UK,
+    JP,
+}
+
+impl AddressType {
+    pub fn from_country_code(s: &str) -> Result<Self, String> {
+        match s {
+            "US" => Ok(Self::US),
+            "UK" => Ok(Self::UK),
+            "JP" => Ok(Self::JP),
+            unknown => Err(format!("unknown address type '{}'", unknown)),
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn to_country_code(&self) -> &'static str {
+        match self {
+            Self::US => "US",
+            Self::UK => "UK",
+            Self::JP => "JP",
+        }
+    }
+
+    pub fn name(&self) -> &'static str {
+        match self {
+            Self::US => "US",
+            Self::UK => "UK",
+            Self::JP => "Japan",
+        }
+    }
+
+    pub fn to_json(&self) -> Value {
+        let description = format!("{} Address", self.name());
+
+        let address_fields = match self {
+            Self::US => vec!["street", "city", "state", "zip"],
+            Self::UK => vec!["buildingName", "street", "city", "county", "postalCode"],
+            Self::JP => vec!["postalCode", "prefecture", "city", "streetNumber"],
+        };
+
+        let props = address_fields.iter().fold(Map::new(), |mut acc, &current| {
+            acc.insert(current.to_owned(), json!({ "type": "string" }));
+            acc
+        });
+
+        json!({
+            "description": description,
+            "type": "object",
+            "properties": props,
+            "required": props.keys().collect::<Vec<_>>()
+        })
+    }
+}
+
+#[schema_macro]
+fn json_address_list(countries: Vec<String>) -> Result<Value, String> {
+    Ok(countries
+        .iter()
+        .map(|country| {
+            let address = AddressType::from_country_code(country).unwrap();
+            address.to_json()
+        })
+        .collect())
+}
+
+fn main() {
+    let schema = eval_schema!(file = "json-schema-macro/tests/schemas/addresses.json").unwrap();
+    let schema_str = serde_json::to_string_pretty(&schema).unwrap();
+    println!("{}", schema_str);
+}
--- json-schema-macro/tests/test.rs
+++ json-schema-macro/tests/test.rs
@@ -1,78 +1,21 @@
 #![forbid(unsafe_code)]
 
 use json_schema_macro::*;
-use serde_json::{json, Map, Value};
-
-enum AddressType {
-    US,
-    UK,
-    JP,
-}
-
-impl AddressType {
-    pub fn from_country_code(s: &str) -> Result<Self, String> {
-        match s {
-            "US" => Ok(Self::US),
-            "UK" => Ok(Self::UK),
-            "JP" => Ok(Self::JP),
-            unknown => Err(format!("unknown address type '{}'", unknown)),
-        }
-    }
-
-    #[allow(dead_code)]
-    pub fn to_country_code(&self) -> &'static str {
-        match self {
-            Self::US => "US",
-            Self::UK => "UK",
-            Self::JP => "JP",
-        }
-    }
-
-    pub fn name(&self) -> &'static str {
-        match self {
-            Self::US => "US",
-            Self::UK => "UK",
-            Self::JP => "Japan",
-        }
-    }
-
-    pub fn to_json(&self) -> Value {
-        let description = format!("{} Address", self.name());
-
-        let address_fields = match self {
-            Self::US => vec!["street", "city", "state", "zip"],
-            Self::UK => vec!["buildingName", "street", "city", "county", "postalCode"],
-            Self::JP => vec!["postalCode", "prefecture", "city", "streetNumber"],
-        };
-
-        let props = address_fields.iter().fold(Map::new(), |mut acc, &current| {
-            acc.insert(current.to_owned(), json!({ "type": "string" }));
-            acc
-        });
-
-        json!({
-            "description": description,
-            "type": "object",
-            "properties": props,
-            "required": props.keys().collect::<Vec<_>>()
-        })
-    }
-}
+use serde_json::json;
 
 #[test]
-fn test_addresses() {
+fn test_message() {
     #[schema_macro]
-    fn json_address_list(countries: Vec<String>) -> Result<Value, String> {
-        Ok(countries
-            .iter()
-            .map(|country| {
-                let address = AddressType::from_country_code(country).unwrap();
-                address.to_json()
-            })
-            .collect())
+    fn fetch_message(_: ()) -> Result<&'static str, String> {
+        Ok("Hello, world!")
     }
 
-    let schema = eval_schema!(file = "json-schema-macro/tests/schemas/addresses.json").unwrap();
-    let schema_str = serde_json::to_string_pretty(&schema).unwrap();
-    println!("{}", schema_str);
+    let schema = eval_schema!(schema = {
+        "message": {
+            "%{fetch_message}%": null
+        }
+    })
+    .unwrap();
+
+    assert_eq!(schema, json!({ "message": "Hello, world!" }));
 }
